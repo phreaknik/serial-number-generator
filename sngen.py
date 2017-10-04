2f4a623807ef42cb82dfb576e97828bfd2377ffc
John Boyd <johnboydiv@gmail.com>
2017-10-04 12:25:21 -0500

    Modified example to generate serial numbers

--- sngen.py
+++ sngen.py
@@ -1,13 +1,18 @@
 import click
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('--model', prompt='Model ID',
+              help='The product model id code.')
+@click.option('--variant', prompt='Variant code',
+              help='The product variant code.')
+@click.option('--first', prompt='First serial',
+              help='The unique serial number to start generation with.')
+@click.option('--count', prompt='Quantity',
+              help='The number of serial numbers to generate.')
+def generateSerials(model, variant, first, count):
+    """Print serial number formatted as <model_id>-<variant_code>-<serial>"""
+    for i in range(int(count)):
+        print(model + '-' + variant + '-' + format(i+int(first), '08'))
 
 if __name__ == '__main__':
-    hello()
+    generateSerials()
